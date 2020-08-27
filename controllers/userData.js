2be6643ca50feb860001fb9e1abd4f0b5abc35f2
himanshu010 <himanshuaswal@yahoo.com>
2020-08-28 02:45:39 +0530

    making checking dynamic

--- controllers/userData.js
+++ controllers/userData.js
@@ -31,7 +31,6 @@ var checkTimeError = function (body) {
         i++;
         var name = x.username;
         if (body[name]) {
-          console.log(name);
           var startTime = parseInt(body.starth) * 60 + parseInt(body.startm);
           var endTime = parseInt(body.endh * 60) + parseInt(body.endm);
           userModel.find({ username: name }, (err, record) => {
@@ -42,7 +41,6 @@ var checkTimeError = function (body) {
               var z;
               for (z of y.schedule) {
                 k++;
-                console.log(z.start);
                 if (
                   (startTime >= z.start && startTime <= z.end) ||
                   (endTime >= z.start && endTime <= z.end) ||
@@ -87,8 +85,7 @@ function insert(body) {
         userNames.push("himanshu");
       }
 
-      var x;
-      for (x of userNames) {
+      for (var x of userNames) {
         const db = client.db(databaseName);
         db.collection("userdatas").update(
           { username: x },
@@ -107,146 +104,134 @@ function insert(body) {
   );
 }
 
-function find_record(body, callback) {
-  var startTime = parseInt(body.starth) * 60 + parseInt(body.startm);
-  var endTime = parseInt(body.endh * 60) + parseInt(body.endm);
-  console.log("aaaa");
-  MongoClient.connect(
-    connectionURL,
-    { useNewUrlParser: true },
-    (error, client) => {
-      if (error) {
-        console.log("Unable to connect to Mongo");
-        return;
+var checkLessThanTwoError = function (body) {
+  return new Promise((resolve, reject) => {
+    userModel.find((err, docs) => {
+      if (err) {
+        reject(err);
       }
-      var userNames = [];
-      if (body.achint) {
-        userNames.push("achint");
+      var cnt = 0,
+        it = 0;
+      for (var x of docs) {
+        it++;
+        if (body[x.username]) {
+          cnt++;
+          if (cnt >= 2) {
+            resolve("More than 2");
+          }
+        }
+        if (it == docs.length) {
+          reject("Less Than 2");
+        }
       }
-      if (body.vishal) {
-        userNames.push("vishal");
-      }
-      if (body.himanshu) {
-        userNames.push("himanshu");
-      }
+    });
+  });
+};
 
-      var x;
-      for (x of userNames) {
-        const db = client.db(databaseName);
-        var cnt = 0;
-        db.collection("userdatas")
-          .find({ username: x })
-          .toArray((error, record) => {
-            var x, y, z;
-            for (x of record) {
-              for (y of x.schedule) {
-                if (
-                  (startTime > y.start && startTime < y.end) ||
-                  (endTime > y.start && endTime < y.end) ||
-                  (startTime < y.start && endTime > y.end)
-                ) {
-                  cnt++;
-                }
-              }
-            }
-            if (cnt != 0) {
-              callback(false);
-            } else {
-              callback(true);
-            }
-          });
-      }
-    }
-  );
-}
+//FIND
 
-function delete_interview(body) {
-  MongoClient.connect(
-    connectionURL,
-    { useNewUrlParser: true },
-    (error, client) => {
-      if (error) {
-        console.log("Unable to connect to Mongo");
-        return;
-      }
-      var userNames = [];
-      if (body.achint) {
-        userNames.push("achint");
-      }
-      if (body.vishal) {
-        userNames.push("vishal");
-      }
-      if (body.himanshu) {
-        userNames.push("himanshu");
-      }
+// function find_record(body, callback) {
+//   var startTime = parseInt(body.starth) * 60 + parseInt(body.startm);
+//   var endTime = parseInt(body.endh * 60) + parseInt(body.endm);
+//   console.log("aaaa");
+//   MongoClient.connect(
+//     connectionURL,
+//     { useNewUrlParser: true },
+//     (error, client) => {
+//       if (error) {
+//         console.log("Unable to connect to Mongo");
+//         return;
+//       }
+//       var userNames = [];
+//       if (body.achint) {
+//         userNames.push("achint");
+//       }
+//       if (body.vishal) {
+//         userNames.push("vishal");
+//       }
+//       if (body.himanshu) {
+//         userNames.push("himanshu");
+//       }
 
-      var x;
-      for (x of userNames) {
-        const db = client.db(databaseName);
-        db.collection("userdatas").update(
-          { username: x },
-          { $pop: { schedule: 1 } }
-        );
-      }
-    }
-  );
-}
+//       var x;
+//       for (x of userNames) {
+//         const db = client.db(databaseName);
+//         var cnt = 0;
+//         db.collection("userdatas")
+//           .find({ username: x })
+//           .toArray((error, record) => {
+//             var x, y, z;
+//             for (x of record) {
+//               for (y of x.schedule) {
+//                 if (
+//                   (startTime > y.start && startTime < y.end) ||
+//                   (endTime > y.start && endTime < y.end) ||
+//                   (startTime < y.start && endTime > y.end)
+//                 ) {
+//                   cnt++;
+//                 }
+//               }
+//             }
+//             if (cnt != 0) {
+//               callback(false);
+//             } else {
+//               callback(true);
+//             }
+//           });
+//       }
+//     }
+//   );
+// }
+
+//DELETE
+
+// function delete_interview(body) {
+//   MongoClient.connect(
+//     connectionURL,
+//     { useNewUrlParser: true },
+//     (error, client) => {
+//       if (error) {
+//         console.log("Unable to connect to Mongo");
+//         return;
+//       }
+//       var userNames = [];
+//       if (body.achint) {
+//         userNames.push("achint");
+//       }
+//       if (body.vishal) {
+//         userNames.push("vishal");
+//       }
+//       if (body.himanshu) {
+//         userNames.push("himanshu");
+//       }
+
+//       var x;
+//       for (x of userNames) {
+//         const db = client.db(databaseName);
+//         db.collection("userdatas").update(
+//           { username: x },
+//           { $pop: { schedule: 1 } }
+//         );
+//       }
+//     }
+//   );
+// }
 
 router.get("/adding", (req, res) => {
-  var delete_record = 0;
-  var insertion_flag = 0;
-  var arr = [];
-  var userNumber = 0;
-  if (req.query.himanshu) {
-    userNumber++;
-  }
-  if (req.query.achint) {
-    userNumber++;
-  }
-  if (req.query.vishal) {
-    userNumber++;
-  }
-
-  if (userNumber < 2) {
-    return res.render("lessthantwo");
-  }
-
-  // find_record(req.query, (val) => {
-  //   console.log(val);
-  //   if (val) {
-  //     try {
-  //       res.render("interviewplaced");
-  //       if (insertion_flag == 0) {
-  //         insert(req.query);
-  //       }
-  //     } catch {
-  //       console.log("already placed");
-  //     }
-
-  //     insertion_flag++;
-  //   } else {
-  //     try {
-  //       res.render("error");
-  //     } catch (e) {
-  //       console.log("error in rendering");
-  //       if (delete_record == 0 && insertion_flag != 0) {
-  //         delete_record++;
-  //         delete_interview(req.query);
-  //         console.log("Deleted");
-  //       } else {
-  //         console.log("already deleted or not inserted earlier");
-  //       }
-  //     }
-  //   }
-  // });
-
-  checkTimeError(req.query)
+  checkLessThanTwoError(req.query)
     .then((result) => {
-      res.render("interviewplaced");
-      insert(req.query);
+      checkTimeError(req.query)
+        .then((result) => {
+          insert(req.query);
+          return res.render("interviewplaced");
+        })
+        .catch((err) => {
+          return res.render("error");
+        });
     })
     .catch((err) => {
-      res.render("error");
+      console.log(err);
+      return res.render("lessthantwo");
     });
 });
 
